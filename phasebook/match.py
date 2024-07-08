8769716126031eba25686a32c8aacc4d89202249
75marsel <jericgappi@gmail.com>
2024-07-08 12:40:08 +0800

    Complete + Bonus Challenge

--- phasebook/match.py
+++ phasebook/match.py
@@ -19,9 +19,19 @@ def match(match_id):
     return {"message": msg, "elapsedTime": end - start}, 200
 
 
-def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
-
-    return True
+def is_match(fave_numbers_1: list, fave_numbers_2:list) -> bool:
+    # set to fast track of exisitng numbers for fave_numbers_2
+    fave_map1 = set(fave_numbers_1)
+    # loop through fave numbers 2
+    for num in fave_numbers_2:
+        # res is false unless there is always a match
+        res = False
+        # check if there is a match between fave numbers 1 and 2
+        if num in fave_map1:
+            # always overwrite the res if there is a match
+            # this will make sure that until the end of loop
+            # the res will always be true
+            res = True
+    
+    # return the current overall match status
+    return res
--- phasebook/search.py
+++ phasebook/search.py
@@ -11,7 +11,7 @@ def search():
     return search_users(request.args.to_dict()), 200
 
 
-def search_users(args):
+def search_users(args: dict) -> list[dict]:
     """Search users database
 
     Parameters:
@@ -24,7 +24,56 @@ def search_users(args):
     Returns:
         a list of users that match the search parameters
     """
-
+ 
     # Implement search here!
+    search_results = [] # empty list
+    
+    # check if the query has age
+    # if it does then change change the type to int
+    if args.get('age'):
+        args['age'] = int(args['age'])
+    
+    # loop through each users
+    for user in USERS:
+        # loop through each query
+        for key in args.keys():
+            if user not in search_results:
+                append_to_list(key, args, user, search_results)
 
+    if search_results:
+        return sort_search(args, search_results)
+
+    # if none of check are passed, return all users
     return USERS
+
+def sort_search(args: dict, search_results: list[dict]) -> list[dict]:
+    """Bonus Challenge
+    
+    sorting method is linear and loops through the arguments
+    where the args' positioning (e.g {'id': '5', 'name': 'Joe', 'age': 30, 'occupation': 'Arc'})
+    will be the basis of sorting priority.
+    
+    [1] anything that matches 'id 5' will be appended first, 
+    [2] then next those who have substring of 'Joe'
+    [3] then those who have age in between/tolerance of -1 and 1 
+    [4] then lastly, check the substring occurence of 'Arc'
+    """
+    sort_result = []
+    
+    for key in args.keys():
+        for user in search_results:
+            if user not in sort_result:
+                append_to_list(key, args, user, sort_result)
+    
+    return sort_result
+
+def append_to_list(key, args: dict, user: dict, target_list: list) -> None:
+    """Function to catch exception and append the user to target list
+        under specific circumstances.
+    """
+    try:
+        if args[key] in user[key]:
+            target_list.append(user)
+    except TypeError:
+        if args['age'] - 1 <= user['age'] <= args['age'] + 1:
+            target_list.append(user)
